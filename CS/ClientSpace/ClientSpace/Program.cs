0ec8c2df8c34f0c2091daee34160d33ef9e9b837
elie-s <e.saide@dq.com.lb>
2024-05-22 11:02:03 +0300

    -added the necessary dlls regarding the progress4gl

--- CS/ClientSpace/ClientSpace/Program.cs
+++ CS/ClientSpace/ClientSpace/Program.cs
@@ -15,11 +15,6 @@ namespace ClientSpace
     {
         static void Main(string[] args)
         {
-            IBLC blc = new BusinessLogic();
-            var credentials = new CredentialsDto { Username = "admin111", Password = "admin", UserID = "234"};
-            bool isAuthenticated  = blc.Authenticate(credentials);
-            Console.WriteLine("Authentication successful: " + isAuthenticated);
-            // Add this line to keep the console window open
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
